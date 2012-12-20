8c1140188871499f81a529344a37aaf91f47d1f0
DerTeufel <dominik-kassel@gmx.de>
2012-12-20 19:13:11 +0100

    fix build

--- Android.mk
+++ Android.mk
@@ -3,6 +3,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
 LOCAL_PACKAGE_NAME := NSTools
