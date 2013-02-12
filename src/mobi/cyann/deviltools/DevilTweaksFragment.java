52eb2c2ab32442b95fcf7b8837c6e60c7f7f078a
DerTeufel <dominik-kassel@gmx.de>
2013-02-12 23:11:54 +0100

    cleaning previous commit + write to devil folder

--- src/mobi/cyann/deviltools/DevilTweaksFragment.java
+++ src/mobi/cyann/deviltools/DevilTweaksFragment.java
@@ -10,18 +10,13 @@ import java.util.regex.Pattern;
 import mobi.cyann.deviltools.PreferenceListFragment.OnPreferenceAttachedListener;
 import mobi.cyann.deviltools.preference.IntegerPreference;
 import mobi.cyann.deviltools.SysCommand;
-import android.app.AlertDialog;
 import android.app.ActivityManager;
 import android.app.ActivityManager.MemoryInfo;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.ListPreference;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
@@ -36,7 +31,6 @@ import android.widget.TextView;
 public class DevilTweaksFragment extends PreferenceListFragment implements OnPreferenceChangeListener {
 	public DevilTweaksFragment() {
 		super(R.layout.devil_tweak);
-		//setOnPreferenceAttachedListener(this);
 	}
 	
 	private ListPreference mBigmem;
@@ -62,43 +56,17 @@ public class DevilTweaksFragment extends PreferenceListFragment implements OnPre
 
 	}
 	
-	/*@Override
-	public void onPreferenceAttached(PreferenceScreen rootPreference, int xmlId) {
-        mBigmem = (ListPreference) findPreference("key_bigmem");
-        mBigmem.setOnPreferenceChangeListener(this);
-	}*/
-
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 	SysCommand sc = SysCommand.getInstance();
 	if (preference == mBigmem) {
             int status = Integer.valueOf((String) objValue);
             int index = mBigmem.findIndexOfValue((String) objValue);
-    	    //private static final String bigmem_string = ((String) objValue);
             mBigmem.setSummary("After Reboot: " + mBigmem.getEntries()[index]);
-	    SysCommand.getInstance().writeSysfs("/sys/kernel/bigmem/enable", ((String) objValue));
+	    sc.writeSysfs("/sys/kernel/bigmem/enable", ((String) objValue));
+	    sc.writeSysfs("/data/local/devil/bigmem", ((String) objValue));
             return true;
         }
 
         return false;
     }
-
-	/*@Override
-	protected void onBindView(View view) {
-		super.onBindView(view);
-		// Sync the summary view
-        TextView summaryView = (TextView) view.findViewById(android.R.id.summary);
-        if (summaryView != null) {
-        	if(value == null) {
-        		summaryView.setText(R.string.status_not_available);
-        	}else if(listValues != null && listValues.length > 0) {
-        		int idx = selectedIndex();
-        		if(idx >= 0) {
-        			summaryView.setText(listLabels[idx]);
-        		}else {
-        			// currently don't know what to do :D
-        			summaryView.setText("?");
-        		}
-        	}
-        }
-	}*/
 }
