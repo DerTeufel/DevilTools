e2a70b96519ed6866dda65aa6905b8b559d215f5
DerTeufel <dominik-kassel@gmx.de>
2013-10-01 21:19:41 +0200

    added RootTools: currently not used

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 roottools
 
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
@@ -18,3 +18,10 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := deviltools
 
 include $(BUILD_PACKAGE)
+
+##################################################
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := roottools:RootTools.jar
+
+include $(BUILD_MULTI_PREBUILT)
